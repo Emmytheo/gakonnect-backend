56758c25d0595e3c062d12da040a800378658dab
emmytheo <chidi.mgbara@gmail.com>
2022-11-17 04:13:02 +0100

    update

--- src/hooks/process-gh-webhooks.js
+++ src/hooks/process-gh-webhooks.js
@@ -10,13 +10,10 @@ module.exports = (options = {}) => {
     return new Promise((resolve, reject) => {
       delete context.data.dateTime;
       let sig = "sha1=" + crypto.createHmac('sha1', process.env.GHWBHK_SECRET).update(JSON.stringify(context.data), 'utf-8').digest('hex');
-      console.log("!!!!", Object.keys(context.data), process.env.GHWBHK_SECRET, context.params.headers['x-hub-signature'], sig);
       if (safeCompare(context.params.headers['x-hub-signature'], sig)) {
-        console.log("it works now !!!!!!!!!!!!!!!");
-        exec('git pull');
+        exec('git pull && npm install && forever restart 0 && sudo fuser -k 443/tcp && forever restart 0');
         context.result = "Recieved"
         resolve(context);
-        // exec('cd ' + repo + ' && git pull && ');
       }
       else{
         reject(new Error("Unauthorized"));
