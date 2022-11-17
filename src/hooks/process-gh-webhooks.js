f46af0d7fcf94f53df84dcc965cb0587101b77db
emmytheo <chidi.mgbara@gmail.com>
2022-11-17 01:54:07 +0100

    update

--- src/hooks/process-gh-webhooks.js
+++ src/hooks/process-gh-webhooks.js
@@ -10,6 +10,7 @@ module.exports = (options = {}) => {
     return new Promise((resolve, reject) => {
       let sig = "sha1=" + crypto.createHmac('sha1', process.env.GHWBHK_SECRET).update(JSON.stringify(context.data)).digest('hex');
       if (safeCompare(context.params.headers['x-hub-signature'], sig)) {
+        console.log("it works !!!!!!!!!!!!!!!");
         exec('git pull && npm install');
         context.result = "Recieved"
         resolve(context);
