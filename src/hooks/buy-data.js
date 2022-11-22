12038bf540641946c0979c42cbf9481adfbf028d
emmytheo <chidi.mgbara@gmail.com>
2022-11-22 17:53:22 +0100

    update

--- src/hooks/buy-data.js
+++ src/hooks/buy-data.js
@@ -6,44 +6,7 @@ const { type } = require('os');
 const axios = require('axios').default;
 var provs = ["ebills", 'subpadi']
 var PROVIDER = provs[0];
-const planNames = {
-  "MTN Data 500MB – 30 Days": '500',
-  "MTN Data 1GB – 30 Days": 'M1024',
-  "MTN Data 2GB – 30 Days": 'M2024',
-  "MTN Data 3GB – 30 Days": '3000',
-  "MTN Data 5GB – 30 Days": '5000',
-  "MTN Data 10GB – 30 Days": '10000',
-  "MTN Data 6GB – 7 Days": 'mtn-20hrs-1500',
-  "MTN Data 30GB – 30 Days": 'mtn-30gb-8000',
-  "MTN Data 40GB – 30 Days": 'mtn-40gb-10000',
-  "MTN Data 75GB – 30 Days": 'mtn-75gb-15000',
-  "Glo Data 1GB – 5 Nights": 'glo100x',
-  "Glo Data 1.25GB – 1 Day (Sunday)": 'glo200x',
-  "Glo Data 1.35GB – 14 Days": 'G500',
-  "Glo Data 5.8GB – 30 Days": 'G2000',
-  "Glo Data 2.9GB – 30 Days": 'G1000',
-  "Glo Data 7.7GB – 30 Days": 'G2500',
-  "Glo Data 10GB – 30 Days": 'G3000',
-  "Glo Data 13.25GB – 30 Days": 'G4000',
-  "Glo Data 18.25GB – 30 Days": 'G5000',
-  "Glo Data 29.5GB – 30 Days": 'G8000',
-  "Glo Data 50GB – 30 Days": 'glo10000',
-  "Airtel Data 750MB – 14 Days": 'airt-500',
-  "Airtel Data 1GB – 1 Day": 'airt-300x',
-  "Airtel Data 1.5GB – 30 Days": 'AIR1000',
-  "Airtel Data 2GB – 2 Days": 'airt-500x',
-  "Airtel Data 2GB – 30 Days": 'airt-1200',
-  "Airtel Data 3GB – 30 Days": 'Air1500',
-  "Airtel Data 4.5GB – 30 Days": 'AIR2000',
-  "Airtel Data 6GB – 7 Days": 'airt-1500-2',
-  "Airtel Data 10GB – 30 Days": 'Air3000',
-  "Airtel Data 20GB – 30 Days": 'Air5000',
-  "Airtel Data 40GB – 30 Days": 'Air100000',
-  "9mobile Data 1GB – 30 Days": '9MOB1000',
-  "9mobile Data 2.5GB – 30 Days": '9MOB34500',
-  "9mobile Data 11.5GB – 30 Days": '9MOB8000',
-  "9mobile Data 15GB – 30 Days": '9MOB5000',
-}
+
 
 // eslint-disable-next-line no-unused-vars
 module.exports = (options = {}) => {
@@ -77,7 +40,7 @@ module.exports = (options = {}) => {
               ebills_config.url = 'https://' + EBILLS.API_BASE_URL + EBILLS.API_BUY_DATA,
               ebills_config.params.phone = context.data.phone;
               ebills_config.params.network_id = context.data.network;
-              ebills_config.params.variation_id = planNames[context.data.dataPlan];
+              ebills_config.params.variation_id = context.data.variation_id;
               console.log(ebills_config);
               //
               axios(ebills_config)
@@ -145,7 +108,7 @@ module.exports = (options = {}) => {
             }
             axios(nearlyfree_config)
             .then(function (response) {
-              console.log(response.data, context.params.user.role);
+              // console.log(response.data, context.params.user.role);
               if(response.data.status === 'successful'){
                 context.data.status = 'successful';
                 context.data.response = response.data.content;
