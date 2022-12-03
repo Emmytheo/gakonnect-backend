f075c15022c5aba4b091319aaff53b3828a77887
emmytheo <chidi.mgbara@gmail.com>
2022-12-03 11:32:11 +0100

    update

--- src/hooks/process-ussd.js
+++ src/hooks/process-ussd.js
@@ -7,7 +7,7 @@ module.exports = (options = {}) => {
     return new Promise((resolve, reject) => {
       if(context.data.serviceCode === "*384*72216#"){
         context.app.service('users').find({query: { 
-          phone : context.data.phoneNumber,
+          phone : "+234" + context.data.phoneNumber.slice(1),
         }})
         .then(res=>{
           if(res.data && res.data.length >= 1){
