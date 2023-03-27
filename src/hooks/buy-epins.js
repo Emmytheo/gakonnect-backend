426ee8e4142c507ccdcb7b1e05711ef247996458
emmytheo <chidi.mgbara@gmail.com>
2023-03-27 18:02:13 +0100

    update

--- src/hooks/buy-epins.js
+++ src/hooks/buy-epins.js
@@ -81,7 +81,7 @@ module.exports = (options = {}) => {
                           }
                           console.log("Delete File successfully.");
                         });
-                        context.result = 'Done'
+                        // context.result = 'Done'
                         resolve(context);
                       }
                       else{
